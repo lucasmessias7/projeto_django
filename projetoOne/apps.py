9b4ca6580f852ec861a24af5238cf3d3f63b6823
Lucas Messias Alves <luquinhasmessias7@gmail.com>
2024-05-02 09:05:50 -0300

    Mudanças-Nome-Pastas

--- projetoOne/apps.py
+++ projetoOne/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CalculadoraConfig(AppConfig):
+class ProjetoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'projeto'
